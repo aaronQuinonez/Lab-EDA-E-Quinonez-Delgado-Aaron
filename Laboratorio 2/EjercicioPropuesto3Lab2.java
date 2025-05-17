ce3d1bd2cad270451564e52c795c447f3fce1deb
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 01:10:17 -0500

    Utilizando recursión en el método trianguloRecursivo1 donde al llegar al caso base, este regresará e imprimirá el triángulo correctamente.

--- Laboratorio 2/EjercicioPropuesto3Lab2.java
+++ Laboratorio 2/EjercicioPropuesto3Lab2.java
@@ -6,14 +6,24 @@ import java.util.Scanner;
 
 public class EjercicioPropuesto3Lab2 {
     public static void trianguloRecursivo1(int base){
-        String triangulo = "";
+        /*String triangulo = "";
         for(int i=0; i<base; i++){
             for(int j=0; j<i+1; j++){
                 triangulo += "*";
             }
             triangulo += "\n";
         }
-        System.out.println(triangulo);
+        System.out.println(triangulo);*/
+        //Caso base
+        if(base == 0){
+            return ;
+        }
+        trianguloRecursivo1(base-1);
+        String str = "";
+        for(int i=0; i<base; i++){
+            str += "*";
+        }
+        System.out.println(str);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
