87d5fe9ba0babbea55dc0bf0e2910097944d5c01
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 00:50:10 -0500

    Creando la estructura inicial del problema, se creó el método trianguloRecursivo1 y en el método main se pide el valor que tendrá "base" y posteriormente ser resuelta en el método.

---
+++ Laboratorio 2/EjercicioPropuesto3Lab2.java
@@ -0,0 +1,17 @@
+/*
+ * Triangulo recursivo 1. El método trianguloRecursivo1 calcula y muestra el resultado.
+ */
+
+import java.util.Scanner;
+
+public class EjercicioPropuesto3Lab2 {
+    public static void trianguloRecursivo1(int base){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese el valor de la base del triángulo:");
+        int base = sc.nextInt();
+        trianguloRecursivo1(base);
+    }
+}
