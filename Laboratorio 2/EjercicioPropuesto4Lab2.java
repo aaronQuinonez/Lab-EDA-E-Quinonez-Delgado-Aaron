2d01b32c0b1f37ba707a11b997b3a2044bd53443
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 01:17:23 -0500

    Creación de la estructura inicial del problema, se pide el valor de la base para crear el triángulo con el método creado trianguloRecursivo2

---
+++ Laboratorio 2/EjercicioPropuesto4Lab2.java
@@ -0,0 +1,18 @@
+/*
+ * Triangulo recursivo 2. El método trianguloRecursivo2 calcula y muestra el resultado
+ */
+
+import java.util.Scanner;
+
+public class EjercicioPropuesto4Lab2 {
+    public static void trianguloRecursivo2(int base){
+        return;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Pedimos el valor de la base
+        System.out.println("Ingrese el valor de la base del triángulo:");
+        int base = sc.nextInt();
+        trianguloRecursivo2(base);
+    }
+}
