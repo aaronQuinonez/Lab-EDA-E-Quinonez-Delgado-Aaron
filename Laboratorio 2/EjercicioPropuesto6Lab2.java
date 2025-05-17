721d0183a57b233b7e98417e94dd2f4f262d05bf
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 03:48:51 -0500

    Culminación del problema, solo se utilizó un bucle for en la recursión, "col" dibujaba los asteriscos en la primera y última columna del cuadrado, mientras que "fila" dibujaba una linea de * en la primera y última fila de acuerdo a la base del cuadrado.

--- Laboratorio 2/EjercicioPropuesto6Lab2.java
+++ Laboratorio 2/EjercicioPropuesto6Lab2.java
@@ -5,17 +5,23 @@
 import java.util.Scanner;
 
 public class EjercicioPropuesto6Lab2 {
+    public static int fila = 0;
     public static void cuadradoRecursivo(int base){
-        for (int i = 0; i < base; i++) {
-            for (int col = 0; col < base; col++) {
-                if (i == 0 || i == base-1 || col == 0 || col == base-1) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println(); // Salto de línea después de cada fila
+        //Caso base
+        if(fila > base-1){
+            return;
         }
+        for (int col = 0; col < base; col++) {
+            if (fila == 0 || fila == base-1 || col == 0 || col == base-1) {
+                System.out.print("*");
+            } else {
+                System.out.print(" ");
+            }
+        }
+        fila++;
+        //Salto de línea después de cada fila
+        System.out.println();
+        cuadradoRecursivo(base);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
