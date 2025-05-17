1dc6f9c21fac29bfc2ae7504c116f0bb9c34de9c
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 15:48:49 -0500

    Agregando archivos del apartado de EjerciciosResueltos.

---
+++ Laboratorio 1/EjercicioResuelto2Lab1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class EjercicioResuelto2Lab1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese un número:");
+        int n = scanner.nextInt();
+
+        int suma = 0;
+        int i = 1;
+
+        while (i <= n) {
+            suma += i;
+            i++;
+        }
+
+        System.out.println("La suma de los primeros " + n + " números naturales es: " + suma);
+        scanner.close();
+    }
+}
