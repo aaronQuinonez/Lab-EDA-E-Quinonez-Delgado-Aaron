29dddea91164610cf8fad2e867cd3b0a252cbf8f
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 02:38:43 -0500

    Probando casos específicos para encontrar una posible solución al problema. Se deduce que el número de espacios en cada fila siempre será de n-1, y el número de * aumenta de 1 en 1.

---
+++ Laboratorio 2/EjercicioPropuesto5Lab2.java
@@ -0,0 +1,17 @@
+/*
+ * Triangulo recursivo 3. El método trianguloRecursivo3 calcula y muestra el resultado.
+ */
+
+import java.util.Scanner;
+
+public class EjercicioPropuesto5Lab2 {
+    public void trianguloRecursivo3(int base){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Triangulo de base 4\n   *\n  * *\n * * *\n* * * *");
+        System.out.println("Triangulo de base 5\n    *\n   * *\n  * * *\n * * * *\n* * * * *");
+
+    }
+}
