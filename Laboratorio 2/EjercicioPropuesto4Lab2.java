4c4b72ff7c67e92075c813977e79cf346e57950a
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-17 02:10:51 -0500

    Probando solución con ciclos for en el método trianguloRecursivo2 para implementar una solución con recursión.

--- Laboratorio 2/EjercicioPropuesto4Lab2.java
+++ Laboratorio 2/EjercicioPropuesto4Lab2.java
@@ -5,14 +5,27 @@
 import java.util.Scanner;
 
 public class EjercicioPropuesto4Lab2 {
-    public static void trianguloRecursivo2(int base){
-        return;
+    public static void trianguloRecursivo2(int base, int espacios){
+        //Usamos i como el número de asteriscos por fila, j los espacios y k imprime los asteriscos correspondientes a i
+        for (int i = 1; i <= base; i++) {
+            // Imprimimos espacios a la izquierda
+            for (int j = 0; j < base - i; j++) {
+                System.out.print(" ");
+            }
+            // Imprimimos asteriscos
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         //Pedimos el valor de la base
         System.out.println("Ingrese el valor de la base del triángulo:");
         int base = sc.nextInt();
-        trianguloRecursivo2(base);
+        //Creamos una variable espacios que determinará el numero de espacios que tendrá en cada fila del triángulo
+        int espacios = 0;
+        trianguloRecursivo2(base , espacios);
     }
 }
