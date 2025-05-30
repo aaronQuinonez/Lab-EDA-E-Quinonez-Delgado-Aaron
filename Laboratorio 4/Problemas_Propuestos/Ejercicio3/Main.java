8ae2e7f4c685d9fa640a2e732a3c86c8d770d515
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-05-30 03:04:29 -0500

    Utilizando LinkedList para crear listas doblemente enlazadas, donde se usa el método add, getFirst, getLast, removeFirst y removeLast.

---
+++ Laboratorio 4/Problemas_Propuestos/Ejercicio3/Main.java
@@ -0,0 +1,28 @@
+package Problemas_Propuestos.Ejercicio3;
+
+import java.util.LinkedList;
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> lista = new LinkedList<>();
+        //INsetando elementos
+        lista.add(1);
+        lista.add(2);
+        lista.add(3);
+        lista.add(4);
+        lista.add(5);
+        lista.add(6);
+        lista.add(7);
+        lista.add(8);
+        lista.add(9);
+        lista.add(10);
+        System.out.println(lista);
+        //Obtener primer y último elemento
+        System.out.println("Primer elemento (head): " + lista.getFirst());
+        System.out.println("Último elemento (tail): " + lista.getLast());
+        // Eliminar desde ambos extremos (requiere conexiones bidireccionales)
+        System.out.println("Eliminando primer elemento: " + lista.removeFirst());
+        System.out.println("Eliminando último elemento: " + lista.removeLast());
+        System.out.println("Nueva lista " + lista);
+        
+    }
+}
