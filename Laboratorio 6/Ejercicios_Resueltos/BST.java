625df8f7db5c5c02cbad6708de0ac98ce11cc95c
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-06-12 23:40:16 -0500

    Agregando método insert para insertar elementos en el árbol binario de búsqueda sin permitir duplicados.

--- Laboratorio 6/Ejercicios_Resueltos/BST.java
+++ Laboratorio 6/Ejercicios_Resueltos/BST.java
@@ -13,10 +13,44 @@ public class BST<E extends Comparable<E>> implements BSTInterface<E>{
     }
 
     @Override
-    public void destroy() {}
+    public void destroy(){}
 
     @Override
-    public void insert(E x) {}
+    public void insert(E x) throws ExceptionItemDuplicate{
+        //Si está vacío
+        if(isEmpty())
+            root = new Node<>(x);
+        else{
+            //Nodo actual
+            Node<E> cur = root;
+            //Nodo padre
+            Node<E> par = null;
+            while(cur != null){
+                int com = x.compareTo(cur.getData());
+                //Si es igual
+                if(com == 0){
+                    throw new ExceptionItemDuplicate("Elemento ya encontrado");
+                }
+                //Guardamos nodo padre
+                par = cur;
+                //Si es menor
+                if(com < 0){
+                    cur = cur.getLeft();
+                }
+                //Si es mayor
+                else{
+                    cur = cur.getRight();
+                }
+            }
+            int com = x.compareTo(par.getData());
+            //Insertamos el nodo
+            if(com > 0){
+                par.setRight(new Node<>(x));
+            }else{
+                par.setLeft(new Node<>(x));
+            }
+        }
+    }
 
     @Override
     public void remove(E x) {}
--- Laboratorio 6/Ejercicios_Resueltos/Main.java
+++ Laboratorio 6/Ejercicios_Resueltos/Main.java
@@ -4,5 +4,13 @@ public class Main {
     public static void main(String[] args) {
         BST<Integer> arbol = new BST<>();
         System.out.println(arbol.isEmpty());
+        try {
+            arbol.insert(15);
+            arbol.insert(10);
+            arbol.insert(7);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        
     }
 }
