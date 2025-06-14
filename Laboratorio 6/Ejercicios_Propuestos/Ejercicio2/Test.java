2fe5f0a13fc36211385e0ca1ff011945125f2ad9
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-06-14 17:57:20 -0500

    Agregar clase Test para interactuar con el árbol BST, permitiendo la inserción de caracteres y mostrando el árbol resultante ASCII.

---
+++ Laboratorio 6/Ejercicios_Propuestos/Ejercicio2/Test.java
@@ -0,0 +1,30 @@
+package Ejercicios_Propuestos.Ejercicio2;
+import Ejercicios_Propuestos.BST;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BST<Integer> arbol = new BST<>();
+        System.out.print("Ingresa una palabra: ");
+        String palabra = scanner.nextLine();
+        try {
+            for (char letra : palabra.toCharArray()) {
+                int ascii = (int) letra;
+                if (!arbol.search(ascii)) {
+                    arbol.insert(ascii);
+                    System.out.println("Insertando letra '" + letra + "' en ASCII: " + ascii);
+                } else {
+                    System.out.println("Letra repetida '" + letra + "', no insertada.");
+                }
+            }
+            System.out.println("\n--- Árbol ---");
+            System.out.println(arbol);
+
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        scanner.close();
+    }
+}
