2be4d9424f4f931457d1cd0ee85b91a5d584bee8
Aarón Quiñonez <aquinonezd@unsa.edu.pe>
2025-06-29 18:12:29 -0500

    Agregar excepciones ExceptionItemDuplicate y ExceptionItemNotFound para manejo de errores en el árbol AVL.

---
+++ Laboratorio7/EjerciciosPropuestos/Ejercicio2/ExceptionItemDuplicate.java
@@ -0,0 +1,7 @@
+package Laboratorio7.EjerciciosPropuestos.Ejercicio2;
+
+public class ExceptionItemDuplicate extends Exception {
+    public ExceptionItemDuplicate(String message) {
+        super(message);
+    }
+}
---
+++ Laboratorio7/EjerciciosPropuestos/Ejercicio2/ExceptionItemNotFound.java
@@ -0,0 +1,7 @@
+package Laboratorio7.EjerciciosPropuestos.Ejercicio2;
+
+public class ExceptionItemNotFound extends Exception {
+    public ExceptionItemNotFound(String message) {
+        super(message);
+    }
+}
